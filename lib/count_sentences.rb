02b6d4b1427cc66d274ec815b7dc41451b8fbe12
gamil91 <alexis_gamil99@yahoo.com.com>
2020-10-08 16:47:43 -0700

    redefined count_sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,21 +15,10 @@ class String
   end
 
   def count_sentences
-    # 0 = looking for punctuation, 1 = looking for space
-    switch = 0
-    sentence_count = 0
-    self.split("").each do |i|
-      if switch == 0
-        if i == "!" || i == "?" || i == "."
-          sentence_count += 1
-          switch = 1
-        end
-      else
-        switch = 0 if switch == 1 && i == " "
-      end
+   
+    self.split(/[.?!]+/).count
+      
     end
-    sentence_count
-  end
   
 
 end
